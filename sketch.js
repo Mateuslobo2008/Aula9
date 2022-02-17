db8e82a0768cbe6c6aba56fea7d03fba5cd2f0f9
Mateuslobo2008 <98826708+Mateuslobo2008@users.noreply.github.com>
2022-02-17 11:30:42 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,13 +1,42 @@
 
 function setup() {
-  createCanvas(400,400)
-  ball=createSprite(50,50,50,50)
+  createCanvas(400,400);
+  background(51);
+  box = createSprite(200,200,30,30);
+
 }
 
 function draw() 
 {
-  background("green");
-drawSprites();
+
+ 
+  
+  
+  
+
+  if (keyIsDown(LEFT_ARROW)) 
+  {
+    background("blue");
+    
+  }
+ 
+    if (keyIsDown(UP_ARROW)) 
+  {
+    background("yellow");
+   
+  }
+
+  if (keyIsDown(DOWN_ARROW)) 
+  {
+    background("green");
+  }
+
+  if(keyIsDown(RIGHT_ARROW))
+  {
+    background("red");
+  }
+  
+  drawSprites();
 }
 
 
